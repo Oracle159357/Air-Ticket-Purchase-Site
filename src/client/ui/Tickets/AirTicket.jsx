2166d5030833ae3960489bdf326fa44a3eaba61c
Misha Rudenko <oracle159357@gmail.com>
2018-09-02 12:26:29 +0300

    #task4 Render filter and checkbox

--- src/client/ui/Tickets/AirTicket.jsx
+++ src/client/ui/Tickets/AirTicket.jsx
@@ -1,6 +1,11 @@
 import React, { PureComponent } from 'react';
 import './AirTicket.scss';
 
+import {
+  Button, ButtonGroup, Container, Row, Col,
+} from 'reactstrap';
+
+import imageCompanyOfTickets from 'images/logoOfCompany.png';
 
 export class AirTickets extends PureComponent {
   constructor(props) {
@@ -50,27 +55,131 @@ export class AirTickets extends PureComponent {
   }
 
   render() {
-    return(
-    <div className="main-background">
-      <div className="mobile-version">
-        <div className="style-logo-company">
-          <img alt="Not Found" src={imageCompanyOfTickets} />
-        </div>
-        <div>
+    return (
+      <div className="main-background">
+        <div className="mobile-version">
+          <div className="style-logo-company">
+            <img alt="Not Found" src={imageCompanyOfTickets} />
+          </div>
           <div>
-            <Button
-              className="show-button max-size-show-filter"
-              onClick={() => this.onClick()}
-              outline
-              color="primary"
-            >
-              Show filter
-            </Button>
-            {' '}
+            <div>
+              <Button
+                className="show-button max-size-show-filter"
+                onClick={() => this.onClick()}
+                outline
+                color="primary"
+              >
+                Show filter
+              </Button>
+              {' '}
+            </div>
           </div>
         </div>
+        <Container>
+          this.state.childVisible
+          ? (
+          <Col className="filter-block" lg={3} md={3} sm={2} xs={12}>
+            <div>
+              <div className="marginDown">
+                ВАЛЮТА
+              </div>
+              <div className="currency-btn-group">
+                <ButtonGroup>
+                  <Button
+                    outline
+                    color="info"
+                    onClick={() => this.onRadioBtnClick('₽')}
+                    active={this.state.currentStateOfButton === '₽'}
+                  >
+                    RUB
+                  </Button>
+                  <Button
+                    outline
+                    color="info"
+                    onClick={() => this.onRadioBtnClick('$')}
+                    active={this.state.currentStateOfButton === '$'}
+                  >
+                    USD
+                  </Button>
+                  <Button
+                    outline
+                    color="info"
+                    onClick={() => this.onRadioBtnClick('€')}
+                    active={this.state.currentStateOfButton === '€'}
+                  >
+                    EUR
+                  </Button>
+                </ButtonGroup>
+              </div>
+              <div>
+                <div className="marginMiddle">
+                  КОЛИЧЕСТВО ПЕРЕСАДОК
+                </div>
+                <div>
+                  <div
+                    className="currency-checkbox custom-control custom-checkbox margin  marginDown"
+                  >
+                    <input
+                      checked={this.state.stateOfCheckBox.length === 4}
+                      onChange={() => this.handleCheckAll()}
+                      type="checkbox"
+                      className="custom-control-input"
+                      id="Checked1"
+                    />
+                    <label className="custom-control-label" htmlFor="Checked1">Все</label>
+                  </div>
+                  <div
+                    className="currency-checkbox custom-control
+                              custom-checkbox margin marginDown"
+                  >
+                    <input
+                      checked={this.state.stateOfCheckBox.includes(0)}
+                      onChange={() => this.handleCheck(0)}
+                      type="checkbox"
+                      className="custom-control-input"
+                      id="Checked2"
+                    />
+                    <label className="custom-control-label" htmlFor="Checked2">Без пересадок</label>
+                  </div>
+                  <div className="currency-checkbox custom-control custom-checkbox margin, marginDown">
+                    <input
+                      checked={this.state.stateOfCheckBox.includes(1)}
+                      onChange={() => this.handleCheck(1)}
+                      type="checkbox"
+                      className="custom-control-input"
+                      id="Checked3"
+                    />
+                    <label className="custom-control-label" htmlFor="Checked3">1 пересадка</label>
+                  </div>
+                  <div className="currency-checkbox custom-control custom-checkbox margin, marginDown">
+                    <input
+                      checked={this.state.stateOfCheckBox.includes(2)}
+                      onChange={() => this.handleCheck(2)}
+                      type="checkbox"
+                      className="custom-control-input"
+                      id="Checked4"
+                    />
+                    <label className="custom-control-label" htmlFor="Checked4">2 пересадки</label>
+                  </div>
+                  <div className="currency-checkbox custom-control custom-checkbox margin, marginDown">
+                    <input
+                      checked={this.state.stateOfCheckBox.includes(3)}
+                      onChange={() => this.handleCheck(3)}
+                      type="checkbox"
+                      className="custom-control-input"
+                      id="Checked5"
+                    />
+                    <label className="custom-control-label" htmlFor="Checked5">3 пересадки</label>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Col>
+          )
+          : null
+          }
+        </Container>
       </div>
-    </div>
     )
   }
 }

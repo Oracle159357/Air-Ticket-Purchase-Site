cf10bae389a0df377a277a6a4e51e239a1b1b159
Misha Rudenko <oracle159357@gmail.com>
2018-09-02 12:11:38 +0300

    #task2 Filter tickets by the number of transfers

--- src/client/ui/Tickets/AirTicket.jsx
+++ src/client/ui/Tickets/AirTicket.jsx
@@ -1,12 +1,15 @@
 import React, { PureComponent } from 'react';
 import './AirTicket.scss';
 
+
 export class AirTickets extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
       currentStateOfButton: '₽',
       currentCurrency: 1,
+      checked: false,
+      stateOfCheckBox: [0, 1, 2, 3],
     };
     this.onRadioBtnClick = this.onRadioBtnClick.bind(this);
   }
@@ -22,6 +25,23 @@ export class AirTickets extends PureComponent {
       this.setState({ currentCurrency: 78 });
     }
     this.setState({ currentStateOfButton });
+  }
+
+  handleCheck(numberOfTransfer) {
+    this.setState((prevState) => {
+      if (prevState.stateOfCheckBox.indexOf(numberOfTransfer) < 0) {
+        return { stateOfCheckBox: [...prevState.stateOfCheckBox, numberOfTransfer] };
+      }
+
+      return { stateOfCheckBox: [...prevState.stateOfCheckBox].filter(element => element !== numberOfTransfer) };
+    }, () => console.log(this.state.stateOfCheckBox));
+  }
+
+  handleCheckAll() {
+    this.setState(
+      prevState => (
+        prevState.stateOfCheckBox.length === 4 ? { stateOfCheckBox: [] } : { stateOfCheckBox: [0, 1, 2, 3] }),
+    );
   }
 
   render() {
